9fc1742e57d5403cafc08f72f4387020c9429268
papa-admin <istraor999@gmail.com>
2021-06-22 16:07:59 +0800

    fix-report

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,47 +2,53 @@
 
 @section('content')
 
-<div class="container  bg-light">
-    <h4 class="py-4 text-center">Welcome back, {{ Auth::user()->name }} !</h4>
-</div>
-<br><br><br>
-<div class="container">
+
+
+<div class="container " style="height: 100vh !important;">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card" id="c_color">
-                <div class="card-header"id="h_color"> <b>DASHBOARD</b> </div>
+        <div class="col-md-12">
+            <div class="container mb-2 bg-light">
+                <h4 class="py-4 text-center">Welcome back, {{ Auth::user()->name }} !</h4>
+            </div>
+            <div class="col-md-6">
+            
+            <div class="card " id="c_color" >
+                <div class="card-header"id="h_color"><i class="fa fa-users-cog"> </i> <b>DASHBOARD</b> </div>
                     <div class="card-body" id="card_some">
                         <div class="container" id="regis_btns">
                             <div class="row justify-content-center"  id="regis_btn">
                                 {{-- <button type="button" id="btn_regis" class="btn btn-danger" onclick="window.location='{{ route('employees') }}'">Register</button> --}}
-                                <button type="button" id="btn_regis" onclick="window.location='{{ route('persons.create') }}'" class="btn btn-danger">Register</button>
+                                <button type="button" id="btn_regis" onclick="window.location='{{ route('persons.create') }}'" class="btn btn-danger"><i class="fa fa-user"> </i> Register</button>
                             </div>
                             </div>
                             <div class="container" id="regis_btns">
                             <div class="row justify-content-center"id="regis_btn">
-                                <button type="button" id="btn_regis" class="btn btn-success" onclick="window.location='{{ route('persons.index') }}'">View</button>
+                                <button type="button" id="btn_regis" class="btn  btn-success" onclick="window.location='{{ route('persons.index') }}'"><i class="fa fa-list"> </i> View</button>
                             </div>
                         </div>
                 </div>
             </div>
-            <br><br>
-            <div class="card" id="c_color">
-                <div class="card-header" id="h_color"> <b>CONTACT TRACING</b> </div>
+        </div>
+            <div class="col-md-6">
+
+                <div class="card " id="c_color">
+                    <div class="card-header" id="h_color"> <i class="fa fa-search"> </i> <b>CONTACT TRACING</b> </div>
                     <div class="card-body" id="card_some">
                         <div class="container" id="regis_btns">
-                        <div class="row justify-content-center" id="regis_btn">
-                            <a type="button" id="btn_regis" class="btn btn-primary" href="search" >Scan For Tracing</a>
-                        </div>
+                            <div class="row justify-content-center" id="regis_btn">
+                                <a type="button" id="btn_regis" class="btn btn-primary" href="search" ><i class="fa fa-qrcode"></i> For Tracing</a>
+                            </div>
                         </div>
                         <div class="container" id="regis_btns">
-                        <div class="row justify-content-center"id="regis_btn">
-                            <a  class="btn btn-info" id="btn_regis" href="reports">Print Reports</a>
-                        </div>
+                            <div class="row justify-content-center"id="regis_btn">
+                                <a  class="btn btn-info" id="btn_regis" href="reports"><i class="fa fa-print"> </i> Print Reports</a>
+                            </div>
                         </div>
                     </div>
+                </div>
             </div>
-
-        </div>
+                
+            </div>
     </div>
 </div>
 
